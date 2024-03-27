65e31de0aec03b13da1f75a5579f07bf10f27801
juan12male <157441751+juan12male@users.noreply.github.com>
2024-03-26 20:40:12 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -7,41 +7,56 @@ usuario=input("Escriba su usuario: ")
 cedula=float(input("Escriba su número de identificación sin espacios y con los ceros:"))
 correoElectronico=input("Ingrese su correo electronico: ")
 
-def calcular_costo(curso, horas):
+def calcularCosto(curso, horas):
     costo_por_hora = 2000 if curso == 'teorico' else 3000
     return costo_por_hora * horas
 
-def registro_usuario():
+def registroUsuario():
     curso = input("¿Qué curso desea tomar? (teorico/practico): ")
 
     if curso == 'practico':
         tipo_vehiculo = input("¿Utilizará vehículo propio o proporcionado? (propio/proporcionado): ")
         horas = int(input("¿Cuántas horas desea contratar? "))
-        costo_total = calcular_costo(curso, horas)
+        costo_total = calcularCosto(curso, horas)
         print(f" Costo total: {costo_total} colones")
     else:
         horas = int(input("¿Cuántas horas desea contratar? "))
-        costo_total = calcular_costo(curso, horas)
+        costo_total = calcularCosto(curso, horas)
         print(f"¡Registro exitoso! Costo total: {costo_total} colones")
     
-    def opcionesAdministrador():
+def opcionesAdministrador():
+    ingresoContrasena=input("Ingrese su contraseña de administrador: ")
+    while ingresoContrasena != contrasena:
+            print("Contraseña incorrecta. Por favor intente nuevamente.")
+            break
+            
+    else:
+        print("Bienvenido a sus opciones de administrador.")
+        print("1. Ver dinero recaudado.\n2. Ver numero de reservas.")
+        opcionAdmin=int(input("Ingrese la opcion deseada:"))
+        if opcionAdmin == 1:
+            print("El dinero recaudado es:",dineroRecaudado)
+        elif opcionAdmin == 2:
+            print("El numero de reservas es:",numeroReservas)
 
-opcion =0
+def facturaElectronica():
+    nombre=input("Por favor ingrese su nombre: ")
+    cedula=float(input("Escriba su número de identificación sin espacios y con los ceros: "))
+    correoElectronico=input("Ingrese su correo electronico: ")
+    file=open("Factura","w")
+    
+
+
+
+    
+opcion = 0
+
 while opcion != 4:
     print("1.Alguna prueba\n2.Dictamen médico\n3.Opciones de administrador")
     opcion=int(input("Seleccione la opción: "))
     if opcion==1:
-        registro_usuario()
+        registroUsuario()
     elif opcion==2:
         break
     elif opcion == 3:
-        
-
-
-
-
-
-
-
-
-
+        opcionesAdministrador()
