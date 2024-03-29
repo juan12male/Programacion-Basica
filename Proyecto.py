648cb0c2d01bda6fc4d591c6ba1c3c8b2feac268
Kenjo-2005 <160413143+Kenjo-2005@users.noreply.github.com>
2024-03-29 12:17:40 -0600

    Update Proyecto.py

--- Proyecto.py
+++ Proyecto.py
@@ -26,54 +26,7 @@ def registroUsuario():
         print(f" Costo total: {costo_total} colones")
         def ingresarEspacio(espacio):
             while True:
-                print("Seleccione el horario que le funcione: \n.Seleccione 4 para reservar de 8am a 9am\n.Seleccione 5 para reservar de 9am a 10am\n.Seleccione 6 para reservar de 10am a 11am\n.Seleccione 7 para reservar de 11am a 12md\n.Seleccione 8 para reservar de 12md a 1pm\n.Seleccione 9 para reservar de 2pm a 3pm\n.Seleccione 10 para reservar de 4pm a 5pm")
-                horario = int(input("Ingrese el número de horario que prefiera:"))
-
-                if horario >=4 and horario<=10:
-                    if espacio[horario-1] == "Vacio":
-                        espacio[horario-1]= nombre
-                        break
-                    else:
-                        print("El horario seleccionado se encuentra ocupado por",nombre)
-                else:
-                    print("El horario seleccionado no existe")
-            return espacio
-
-        def liberarHorario(espacio):
-            while True:
-                horario = int(input("Ingrese el numero de horario que selecciono:"))
-
-                if horario >=4 and horario<=10:
-                    if espacio[horario-1] == "Vacio":
-                        print("El horario seleccioando esta vacio")
-                    else:
-                        espacio[horario-1]= "Vacio"
-                        print("El horario seleccionado anteriormente ha sido liberado")
-                        break
-                else:
-                    print("El horario seleccionado no existe")
-            return espacio
-
-        def mostrar(espacio):
-            for x in range(len(espacio)):
-                if espacio[x] == "Vacio":
-                    print("El horario",x+1,"esta vacio")
-                else:
-                    print("El horario",x+1,"esta ocupado por:",espacio[x])
-        opcion =1
-        while opcion !=3:
-            print("1.Reservar horario\n2.Liberar horario\n3.Salir")
-            opcion = int(input("Seleccione la opción que desea:"))
-            if opcion ==1:
-                espacio = ingresarEspacio(espacio)
-                mostrar(espacio)
-            elif opcion ==2:
-                espacio =liberarHorario(espacio)
-                mostrar(espacio)
-            elif opcion==3:
-                print("Mucha gracias")
-            else:
-                print("Opcion incorrecta")
+                print("Seleccione el horario que le funcione:")
     else:
         horas = int(input("¿Cuántas horas desea contratar? "))
         costo_total = calcularCosto(curso, horas)
